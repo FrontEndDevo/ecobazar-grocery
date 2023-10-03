8934c5380a8069e0f962616ab6ff98ef51ac097d
FrontEndDevo <mylinkacco@gmail.com>
2023-10-03 23:47:13 +0300

    Centering the container class in TailwindCSS by default without needing to add mx-auto class

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-red-400">Hello World</h1>
+      <h1 className="text-red-400 container">Hello World</h1>
     </>
   );
 }
