535979deb5df9eca357c3cc3673f2c5ea94c330a
FrontEndDevo <mylinkacco@gmail.com>
2023-10-03 23:57:36 +0300

    Add our main colors in tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // For (gray and neutral) colors:
+        main: {
+          100: "#9CA3AF",
+          500: "#374151",
+          700: "#262626",
+        },
+        // For (green) colors:
+        primary: {
+          100: "#84D187",
+          500: "#00B207",
+          700: "#2C742F",
+        },
+      },
+    },
 
     container: {
       center: true,
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 };
