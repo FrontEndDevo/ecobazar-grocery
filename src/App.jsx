911880070c00653e10dfad8f2fa3d0b58d6afb38
FrontEndDevo <mylinkacco@gmail.com>
2023-10-04 00:23:04 +0300

    Install React-Router-Dom v6 & create and finish Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+const homeRouter = (
+  <>
+    <Navbar />
+    <Header />
+  </>
+);
 
 export default function App() {
   return (
     <>
-      <Header />
+      <Routes>
+        <Route path="/" element={homeRouter} />
+      </Routes>
     </>
   );
 }
