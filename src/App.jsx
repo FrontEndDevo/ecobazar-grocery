abb4024bf3754576ec14f3c4fa154933a356c9bf
FrontEndDevo <mylinkacco@gmail.com>
2023-10-04 17:00:22 +0300

    Create WhyChooseUs component and finish it

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Benefits from "./components/Benefits";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 const homeRouter = (
   <>
     <Navbar />
     <Header />
     <Benefits />
+    <WhyChooseUs />
   </>
 );
 
