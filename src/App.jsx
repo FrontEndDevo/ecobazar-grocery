5dc7e5f1490d919de880f3a52378ec96d8eaa552
FrontEndDevo <mylinkacco@gmail.com>
2023-10-07 14:56:42 +0300

    Fetch the data from firestore correctly after secured the info from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
@@ -5,6 +6,9 @@ import Benefits from "./components/Benefits";
 import WhyChooseUs from "./components/WhyChooseUs";
 import Delivery from "./components/Delivery";
 import Footer from "./components/Footer";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
+import { useEffect } from "react";
 
 const homeRouter = (
   <>
@@ -17,7 +21,23 @@ const homeRouter = (
   </>
 );
 
-export default function App() {
+const App = () => {
+  useEffect(() => {
+    // Store the Restaurants.
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "restaurants"));
+      const arr = [];
+
+      querySnapshot.forEach((doc) => {
+        arr.push(doc.data());
+      });
+
+      console.log(arr);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Routes>
@@ -25,4 +45,6 @@ export default function App() {
       </Routes>
     </>
   );
-}
+};
+
+export default App;
