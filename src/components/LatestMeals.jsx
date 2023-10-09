dd1329f26bc6f1f76cf13ed4b7a1dc1e817e5ed6
FrontEndDevo <mylinkacco@gmail.com>
2023-10-09 16:54:19 +0300

    Install Axios and fetch the meals

--- src/components/LatestMeals.jsx
+++ src/components/LatestMeals.jsx
@@ -1,4 +1,24 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const LatestMeals = () => {
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      await axios
+        .get("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
+        .then((data) => {
+          setMeals(data?.data);
+        })
+        .catch((err) => {
+          console.log(`Something went wrong! ${err}`);
+        });
+    };
+    fetchMeals();
+  }, []);
+
+  console.log(meals);
+
   return <div>LatestMeals</div>;
 };
 
