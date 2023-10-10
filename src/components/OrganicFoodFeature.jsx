b736fb0e771c1a92a1789c2252717fc6692b940b
FrontEndDevo <mylinkacco@gmail.com>
2023-10-10 16:02:58 +0300

    Finish the OrganicFoodFeature component styles with Responsive-Design

--- src/components/OrganicFoodFeature.jsx
+++ src/components/OrganicFoodFeature.jsx
@@ -22,13 +22,20 @@ const foodFeatures = [
 
 const OrganicFoodFeature = () => {
   const ourFeatures = (
-    <div>
+    <div className="my-8">
       {foodFeatures.map((feature, index) => (
-        <div key={index}>
-          <FontAwesomeIcon icon={faCircleCheck} />
+        <div key={index} className="flex gap-4 my-6 text-center lg:text-start">
+          <FontAwesomeIcon
+            icon={faCircleCheck}
+            className="hidden text-xl text-primary-700 lg:block"
+          />
           <div>
-            <h4>{feature.title}</h4>
-            <p>{feature.description}</p>
+            <h4 className="mb-2 text-base font-bold text-main-700">
+              {feature.title}
+            </h4>
+            <p className="text-sm text-justify text-main-100">
+              {feature.description}
+            </p>
           </div>
         </div>
       ))}
@@ -36,23 +43,49 @@ const OrganicFoodFeature = () => {
   );
 
   return (
-    <section>
-      <div>
+    <section className="relative py-20 bg-slate-50">
+      <div className="container flex flex-col items-center xl:mx-0 xl:flex-row xl:gap-40 2xl:gap-10">
         <div>
-          <img src={imageOne} alt="Organic Food image 1" />
-          <img src={imageTwo} alt="Organic Food image 2" />
+          <div className="flex flex-col items-center gap-12 md:flex-row">
+            <img
+              src={imageOne}
+              alt="Organic Food image 1"
+              className="w-full xl:w-80"
+            />
+            <img
+              src={imageTwo}
+              alt="Organic Food image 2"
+              className="w-full xl:w-96"
+            />
+          </div>
+          <div>
+            <img
+              src={backgroundOne}
+              alt="Organic Food background 1"
+              className="absolute left-0 w-28 top-12"
+            />
+            <img
+              src={backgroundTwo}
+              alt="Organic Food background 2"
+              className="absolute w-28 left-12 bottom-12"
+            />
+            <img
+              src={backgroundThree}
+              alt="Organic Food background 3"
+              className="absolute top-0 w-28 right-12"
+            />
+          </div>
         </div>
-        <img src={backgroundOne} alt="Organic Food background 1" />
-        <img src={backgroundTwo} alt="Organic Food background 2" />
-        <img src={backgroundThree} alt="Organic Food background 3" />
-      </div>
 
-      <div>
-        <h2>100% Trusted Organic Food Store</h2>
-        {ourFeatures}
-        <button className="flex items-center justify-center gap-4 px-6 py-4 text-white duration-200 rounded-full lg:px-12 bg-primary-500 hover:bg-primary-700">
-          Shop Now <FontAwesomeIcon icon={faArrowRight} />
-        </button>
+        <div className="z-40 mx-4 mt-8 lg:mt-0 xl:mx-0 basis-1/2">
+          <h2 className="text-xl font-bold text-center md:text-2xl lg:text-4xl xl:text-start text-main-700">
+            100% Trusted Organic Food Store
+          </h2>
+          {ourFeatures}
+          <button className="flex items-center justify-center w-full gap-4 px-6 py-4 text-white duration-200 rounded-full lg:w-fit lg:px-12 bg-primary-500 hover:bg-primary-700">
+            Shop Now <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        </div>
       </div>
     </section>
   );
