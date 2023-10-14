db5b9f7ff93b99947325f317628e222d0a19c9a9
FrontEndDevo <mylinkacco@gmail.com>
2023-10-14 15:23:24 +0300

    Fetch all meals in all alphabets from themealdb.com in ShopPage

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,6 +1,28 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import axios from "axios";
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const ShopPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchAllMealsWithAllLetters = async () => {
+      alphabet.split("").map(async (letter) => {
+        await axios
+          .get(`${import.meta.env.VITE_MEALS_BY_FIRST_LETTER}=${letter}`)
+          .then((res) => {
+            console.log(res.data);
+          })
+          .catch((err) => {
+            throw new Error(`Something went wrong! Error:${err}`);
+          });
+      });
+    };
+    fetchAllMealsWithAllLetters();
+  }, []);
+
   return (
     <>
       <Navbar />
