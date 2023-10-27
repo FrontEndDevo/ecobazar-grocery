c9aebfe954c1471f714a20a307fb21872a76fb16
FrontEndDevo <mylinkacco@gmail.com>
2023-10-27 20:57:01 +0200

    Replace useState with useReducer in shopFilter component to use more complex logic

--- src/components/ShopFilter.jsx
+++ src/components/ShopFilter.jsx
@@ -2,14 +2,41 @@ import { useSelector } from "react-redux";
 import { alphabet } from "../pages/ShopPage";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useReducer } from "react";
+
+const filterReducer = {
+  openLetters: false,
+  openAreas: false,
+  openCategories: false,
+};
+
+const filterReducerFn = (state, action) => {
+  switch (action.type) {
+    case "OPEN_LETTERS":
+      return {
+        ...state,
+        openLetters: !state.openLetters,
+      };
+    case "OPEN_AREAS":
+      return {
+        ...state,
+        openAreas: !state.openAreas,
+      };
+    case "OPEN_CATEGORIES":
+      return {
+        ...state,
+        openCategories: !state.openCategories,
+      };
+
+    default:
+      return { ...state };
+  }
+};
 
 const ShopFilter = () => {
-  const [letters, setLetters] = useState(false);
-  const [areas, setAreas] = useState(false);
-  const [categories, setCategories] = useState(false);
+  const [filters, dispatch] = useReducer(filterReducerFn, filterReducer);
 
-  const filters = {
+  const filtersOptions = {
     letters: alphabet,
     categories: [],
     areas: [],
@@ -19,23 +46,33 @@ const ShopFilter = () => {
   allMeals.forEach((mealArr) => {
     mealArr.meals.forEach((meal) => {
       // Get areas.
-      if (!filters.areas.includes(meal.strArea)) {
-        filters.areas.push(meal.strArea);
+      if (!filtersOptions.areas.includes(meal.strArea)) {
+        filtersOptions.areas.push(meal.strArea);
       }
 
       // Get categories.
-      if (!filters.categories.includes(meal.strCategory)) {
-        filters.categories.push(meal.strCategory);
+      if (!filtersOptions.categories.includes(meal.strCategory)) {
+        filtersOptions.categories.push(meal.strCategory);
       }
     });
   });
 
+  // Track if a checkbox is checked or not.
+  const changeLetterCheckboxHandler = (val) => {
+    console.log(val);
+  };
+
   // Render all filter arrays:
   const ShopFilterByLetters = (
     <ul className="grid grid-cols-5 mt-4 md:grid-cols-2">
-      {filters.letters.split("").map((item, i) => (
+      {filtersOptions.letters.split("").map((item, i) => (
         <li key={i} className="flex items-center gap-2 my-1">
-          <input type="checkbox" name="input-Checkbox" id="checkbox" />
+          <input
+            type="checkbox"
+            name="input-Checkbox"
+            id="checkbox"
+            onChange={() => changeLetterCheckboxHandler(item)}
+          />
           <label htmlFor="checkbox">{item}</label>
         </li>
       ))}
@@ -44,7 +81,7 @@ const ShopFilter = () => {
 
   const ShopFilterByAreas = (
     <ul className="grid grid-cols-2 mt-4 md:grid-cols-1">
-      {filters.areas.map((item, i) => (
+      {filtersOptions.areas.map((item, i) => (
         <li key={i} className="flex items-center gap-2 my-1">
           <input type="checkbox" name="input-Checkbox" id="checkbox" />
           <label htmlFor="checkbox">{item}</label>
@@ -55,7 +92,7 @@ const ShopFilter = () => {
 
   const ShopFilterByCategories = (
     <ul className="grid grid-cols-2 mt-4 md:grid-cols-1">
-      {filters.categories.map((item, i) => (
+      {filtersOptions.categories.map((item, i) => (
         <li key={i} className="flex items-center gap-2 my-1">
           <input type="checkbox" name="input-Checkbox" id="checkbox" />
           <label htmlFor="checkbox">{item}</label>
@@ -66,15 +103,15 @@ const ShopFilter = () => {
 
   // Btns to control toggling filters:
   const toggleLetterHandler = () => {
-    setLetters((prevState) => !prevState);
+    dispatch({ type: "OPEN_LETTERS" });
   };
 
   const toggleAreaHandler = () => {
-    setAreas((prevState) => !prevState);
+    dispatch({ type: "OPEN_AREAS" });
   };
 
   const toggleCategoryHandler = () => {
-    setCategories((prevState) => !prevState);
+    dispatch({ type: "OPEN_CATEGORIES" });
   };
 
   const keywordClasses =
@@ -88,11 +125,11 @@ const ShopFilter = () => {
           <FontAwesomeIcon
             icon={faChevronDown}
             className={`text-xl duration-300 text-main-500 ${
-              letters && "-rotate-180"
+              filters.openLetters && "-rotate-180"
             }`}
           />
         </span>
-        {letters && ShopFilterByLetters}
+        {filters.openLetters && ShopFilterByLetters}
       </div>
 
       <div className="p-2 mb-2">
@@ -101,11 +138,11 @@ const ShopFilter = () => {
           <FontAwesomeIcon
             icon={faChevronDown}
             className={`text-xl duration-300 text-main-500 ${
-              areas && "-rotate-180"
+              filters.openAreas && "-rotate-180"
             }`}
           />
         </span>
-        {areas && ShopFilterByAreas}
+        {filters.openAreas && ShopFilterByAreas}
       </div>
 
       <div className="p-2 mb-2">
@@ -114,11 +151,11 @@ const ShopFilter = () => {
           <FontAwesomeIcon
             icon={faChevronDown}
             className={`text-xl duration-300 text-main-500 ${
-              categories && "-rotate-180"
+              filters.openCategories && "-rotate-180"
             }`}
           />
         </span>
-        {categories && ShopFilterByCategories}
+        {filters.openCategories && ShopFilterByCategories}
       </div>
 
       <button className="w-full p-2 mt-4 text-xl font-bold duration-300 border-2 border-blue-900 rounded-full hover:text-white hover:bg-blue-900 text-main-700">
