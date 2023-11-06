0ed52983ff75e0311f0fc3a3c3d3d90a74248547
FrontEndDevo <mylinkacco@gmail.com>
2023-11-06 15:26:40 +0200

    Delete JSON file (Vegetables.json) because we stored it in Firebase & create and style headers of AllProducts component

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,17 +1,35 @@
+import { Link } from "react-router-dom";
+
+const kindsOfProducts = [
+  "vegetable",
+  "fruit",
+  "meat & fish",
+  "drinks",
+  "view all",
+];
 const AllProducts = () => {
+  const allKindsOfProducts = (
+    <ul className="flex items-center justify-center gap-10">
+      {kindsOfProducts.map((item, i) => (
+        <li
+          key={i}
+          className="text-lg capitalize duration-300 text-main-100 hover:text-green-700"
+        >
+          <Link>{item}</Link>
+        </li>
+      ))}
+    </ul>
+  );
   return (
-    <section>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex tempora nulla
-      voluptatem quo? Ullam odio vero consequuntur asperiores facere sit facilis
-      consequatur, debitis nulla. Dolor amet eaque, cumque, at sunt quam
-      laboriosam eos earum, voluptate et provident molestias doloremque
-      voluptatibus voluptatem nam magni dolorum. Dolores, enim pariatur odit
-      porro, accusantium facilis sapiente blanditiis exercitationem quaerat
-      harum qui ducimus. Rem nobis quos impedit, atque nemo corrupti, laudantium
-      aliquid sit vitae quisquam porro quidem. Quas, rerum! Voluptate, debitis
-      quo ut ipsa laborum fugiat nihil excepturi illo vero ratione, repudiandae
-      labore iure, est hic ullam incidunt delectus cupiditate quisquam
-      reprehenderit. Velit, temporibus quidem!
+    <section className="py-20 bg-green-50">
+      <div className="container">
+        <div className="text-center">
+          <h2 className="mb-10 font-bold capitalize text-7xl">
+            Introducing our products
+          </h2>
+          {allKindsOfProducts}
+        </div>
+      </div>
     </section>
   );
 };
