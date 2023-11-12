f5378c25a6e778252a1285080dbadfba7e724d64
FrontEndDevo <mylinkacco@gmail.com>
2023-11-12 17:08:09 +0200

    Create vegetables slice and subscribe for it in Redux store.

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,13 @@ import fruitsSlice from "./slices/fruitsSlice";
 import mealsSlice from "./slices/mealsSlice";
 import deliciousMealsSlice from "./slices/deliciousMealsSlice";
 import mealsCategoriesSlice from "./slices/mealsCategoriesSlice";
+import vegetablesSlice from "./slices/vegetablesSlice";
 
 const store = configureStore({
   reducer: {
     restaurants: restaurantsSlice,
     fruits: fruitsSlice,
+    vegetables: vegetablesSlice,
     meals: mealsSlice,
     deliciousMeals: deliciousMealsSlice,
     mealsCategories: mealsCategoriesSlice,
