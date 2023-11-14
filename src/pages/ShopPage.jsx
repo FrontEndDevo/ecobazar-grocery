3b18d051e4f2bd4e3d57215c757a0686a0987a86
FrontEndDevo <mylinkacco@gmail.com>
2023-11-14 23:44:40 +0200

    Check first if there are meals before fetching them so as not to send request in vain in ShopPage.jsx

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { mealsActions } from "../redux/slices/mealsSlice";
@@ -9,27 +9,32 @@ export const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const ShopPage = () => {
   const [mealsError, setMealsError] = useState(null);
 
+  const reduxMeals = useSelector((state) => state.meals);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchAllMealsWithAllLetters = async () => {
-      alphabet.split("").map(async (letter) => {
-        await axios
-          .get(`${import.meta.env.VITE_MEALS_BY_FIRST_LETTER}=${letter}`)
-          .then((res) => {
-            dispatch(
-              mealsActions.addNewMealType({
-                letter,
-                meals: res.data.meals || [],
-              })
-            );
-          })
-          .catch((err) => {
-            setMealsError(`Something went wrong! Error:${err}`);
-          });
-      });
-    };
-    fetchAllMealsWithAllLetters();
+    // Check first if there are meals in redux store so not to send request in vain.
+    if (reduxMeals.totalNumOfMeals == 0) {
+      const fetchAllMealsWithAllLetters = async () => {
+        alphabet.split("").map(async (letter) => {
+          await axios
+            .get(`${import.meta.env.VITE_MEALS_BY_FIRST_LETTER}=${letter}`)
+            .then((res) => {
+              dispatch(
+                mealsActions.addNewMealType({
+                  letter,
+                  meals: res.data.meals || [],
+                })
+              );
+            })
+            .catch((err) => {
+              setMealsError(`Something went wrong! Error:${err}`);
+            });
+        });
+      };
+      fetchAllMealsWithAllLetters();
+    }
   }, []);
 
   return (
