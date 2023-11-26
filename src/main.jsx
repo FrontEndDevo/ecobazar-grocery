0413719d60551e5ca7d158edbba0fcb226cc23b4
FrontEndDevo <mylinkacco@gmail.com>
2023-11-26 13:12:00 +0200

    Set all configrations for React-persist package

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,18 @@ import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,16 +5,31 @@ import mealsSlice from "./slices/mealsSlice";
 import deliciousMealsSlice from "./slices/deliciousMealsSlice";
 import mealsCategoriesSlice from "./slices/mealsCategoriesSlice";
 import vegetablesSlice from "./slices/vegetablesSlice";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+
+// Prepare all slices in reducers constant:
+const reducers = combineReducers({
+  restaurants: restaurantsSlice,
+  fruits: fruitsSlice,
+  vegetables: vegetablesSlice,
+  meals: mealsSlice,
+  deliciousMeals: deliciousMealsSlice,
+  mealsCategories: mealsCategoriesSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    restaurants: restaurantsSlice,
-    fruits: fruitsSlice,
-    vegetables: vegetablesSlice,
-    meals: mealsSlice,
-    deliciousMeals: deliciousMealsSlice,
-    mealsCategories: mealsCategoriesSlice,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 export default store;
