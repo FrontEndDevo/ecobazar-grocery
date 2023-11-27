01558fd1c7003c84584c98a2b6609cd6a15d0eac
FrontEndDevo <mylinkacco@gmail.com>
2023-11-27 16:55:45 +0200

    Move all logic of fetching meals from ShopPage to Homepage to get all data required when the main page is loaded

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,8 +10,47 @@ import OrganicFoodFeature from "../components/OrganicFoodFeature";
 import FoodProducts from "../components/FoodProducts";
 import DeliciousMeals from "../components/DeliciousMeals";
 import AllProducts from "../components/AllProducts/AllProducts";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { mealsActions } from "../redux/slices/mealsSlice";
+import axios from "axios";
+
+export const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+
+  const reduxMeals = useSelector((state) => state.meals);
+
+  useEffect(() => {
+    // Check first if there are meals in redux store so not to send request in vain.
+    if (reduxMeals.totalNumOfMeals == 0) {
+      const fetchAllMealsWithAllLetters = async () => {
+        alphabet.split("").map(async (letter) => {
+          await axios
+            .get(`${import.meta.env.VITE_MEALS_BY_FIRST_LETTER}=${letter}`)
+            .then((res) => {
+              dispatch(
+                mealsActions.addNewMealType({
+                  letter,
+                  meals: res.data.meals || [],
+                })
+              );
+            })
+            .catch((err) => {
+              dispatch(
+                errorsActions.addError({
+                  errorType: "meals",
+                  errorMessage: `Something went wrong! Error:${err}`,
+                })
+              );
+            });
+        });
+      };
+      fetchAllMealsWithAllLetters();
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
