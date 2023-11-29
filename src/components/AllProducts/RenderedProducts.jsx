53d79e39422486f3066d7a3eefa9c20f4c4acbee
FrontEndDevo <mylinkacco@gmail.com>
2023-11-29 14:07:32 +0200

    Add simple animation for icons in each product rendered in RenderedProducts.jsx

--- src/components/AllProducts/RenderedProducts.jsx
+++ src/components/AllProducts/RenderedProducts.jsx
@@ -1,8 +1,19 @@
 import { faEye, faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faBagShopping, faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 const RenderedProducts = ({ productType }) => {
+  const [hoverdProductId, setHoverdProductId] = useState(null);
+
+  const enterProductHandler = (id) => {
+    setHoverdProductId(id);
+  };
+
+  const leaveProductHandler = () => {
+    setHoverdProductId(null);
+  };
+
   // Render all products (vegetables, fruits, ...etc):
   const renderedProducts = productType.target.slice(0, 9).map((item, i) => {
     // Detect the kind of products(vegetables, fruits, meals,...etc), to render correct title & img:
@@ -42,7 +53,9 @@ const RenderedProducts = ({ productType }) => {
     return (
       <li
         key={i}
-        className="relative flex items-center w-full gap-10 p-3 bg-white border h-44"
+        className="relative overflow-hidden rounded-lg flex duration-200 items-center w-full gap-10 p-3 bg-white border-2 h-44  hover:border-green-500"
+        onMouseEnter={() => enterProductHandler(i)}
+        onMouseLeave={leaveProductHandler}
       >
         <img src={img} alt={title} className="w-1/3 h-full" />
 
@@ -68,15 +81,21 @@ const RenderedProducts = ({ productType }) => {
         <div className="absolute flex items-end gap-4 text-xl lg:text-2xl bottom-4 right-4">
           <FontAwesomeIcon
             icon={faBagShopping}
-            className="mt-4 text-green-400 duration-200 cursor-pointer hover:text-green-800 hover:-translate-y-3"
+            className={`mt-4 text-green-400 duration-200 cursor-pointer hover:text-green-800 hover:-translate-y-3 opacity-0 -translate-x-12 ${
+              hoverdProductId == i && "translate-x-0 opacity-100"
+            }`}
           />
           <FontAwesomeIcon
             icon={faEye}
-            className="text-blue-400 duration-200 cursor-pointer hover:text-blue-800 hover:-translate-y-3"
+            className={`text-blue-400 duration-200 cursor-pointer hover:text-blue-800 hover:-translate-y-3 opacity-0 translate-y-12 ${
+              hoverdProductId == i && "translate-y-0 opacity-100"
+            }`}
           />
           <FontAwesomeIcon
             icon={faHeart}
-            className="text-red-400 duration-200 cursor-pointer hover:text-red-800 hover:-translate-y-3"
+            className={`text-red-400 duration-200 cursor-pointer opacity-0 translate-x-12 hover:text-red-800 hover:-translate-y-3 ${
+              hoverdProductId == i && "translate-x-0  opacity-100"
+            }`}
           />
         </div>
       </li>
