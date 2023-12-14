a8f170d8dea1b702c95bef7eb70a8c9579ae7eb7
FrontEndDevo <mylinkacco@gmail.com>
2023-12-14 19:20:47 +0200

    Rendering all letters and get the selected letter using useState hook again in the new ShopFilter.jsx

--- src/components/Shop/ShopFilter.jsx
+++ src/components/Shop/ShopFilter.jsx
@@ -1,34 +1,54 @@
 import { useState } from "react";
 import { alphabet } from "../../pages/HomePage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const ShopFilter = () => {
-  const [letter, setLetter] = useState(null);
+  // These states for letters.
+  const [openLetters, setOpenLetters] = useState(false);
+  const [filterLetters, setFilterLetters] = useState([]);
 
-  const selectLetterHandler = (letter) => {
-    setLetter(letter.target.value);
+  const chooseLetterHandler = (letter) => {
+    // Check if the letter is exist in the array or not to add/remove it.
+    filterLetters.includes(letter)
+      ? setFilterLetters(filterLetters.filter((item) => item != letter))
+      : setFilterLetters([...filterLetters, letter]);
   };
 
   const lettersOptions = (
-    <div>
-      <select
-        name="letters"
-        id="letters"
-        className="p-2 text-center duration-300 border-2 rounded-lg outline-none cursor-pointer text-main-700 hover:border-primary-700 focus:border-primary-700"
-        onChange={selectLetterHandler}
+    <div className="relative">
+      <div
+        onClick={() => setOpenLetters((prevState) => !prevState)}
+        className={`flex items-center hover:text-main-700 justify-between w-40 px-3 py-2 duration-300  border-2 rounded-lg outline-none cursor-pointer bg-white ${
+          openLetters ? "text-main-700 border-primary-700" : "text-neutral-500"
+        }`}
       >
-        <option disabled selected value="letter">
-          Letter
-        </option>
-        {alphabet.split("").map((letter, i) => (
-          <option key={i} value={letter} className="">
-            {letter}
-          </option>
-        ))}
-      </select>
+        <button>Select Letter</button>
+        <FontAwesomeIcon icon={openLetters ? faChevronUp : faChevronDown} />
+      </div>
+      {openLetters && (
+        <ul className="absolute w-40 p-1 overflow-hidden overflow-y-auto duration-200 bg-white border rounded-lg h-44 top-12">
+          {alphabet.split("").map((letter, i) => (
+            <li
+              key={i}
+              className={`px-2 py-1 my-1 duration-200 cursor-pointer hover:bg-primary-100 text-main-700 ${
+                filterLetters.includes(letter) && "bg-primary-100 font-bold"
+              }`}
+              onClick={() => chooseLetterHandler(letter)}
+            >
+              {letter}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 
-  return <section className="container py-10">{lettersOptions}</section>;
+  return (
+    <section className="pt-6 pb-24 bg-neutral-100">
+      <div className="container">{lettersOptions}</div>
+    </section>
+  );
 };
 
 export default ShopFilter;
