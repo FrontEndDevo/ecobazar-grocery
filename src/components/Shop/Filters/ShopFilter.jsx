a73fe40b1b29ff4e19adfa48a44e95eee4755ed7
FrontEndDevo <mylinkacco@gmail.com>
2023-12-16 00:30:43 +0200

    Handling positions of filters components in ShopFilter

--- src/components/Shop/Filters/ShopFilter.jsx
+++ src/components/Shop/Filters/ShopFilter.jsx
@@ -1,10 +1,17 @@
 import Letter from "./Letter";
+import Price from "./Price";
+import Category from "./Category";
 
 const ShopFilter = () => {
   return (
     <section className="pt-6 pb-24 bg-neutral-100">
-      <div className="container">
-        <Letter />
+      <div className="container flex items-center justify-between gap-20">
+        <div className="flex items-center gap-10">
+          <Letter />
+        </div>
+        <div>
+          <Price />
+        </div>
       </div>
     </section>
   );
