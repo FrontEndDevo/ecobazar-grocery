635d40044defcdc227bd9609439cc432973f2d5a
FrontEndDevo <mylinkacco@gmail.com>
2023-12-16 01:03:59 +0200

    Handling props and which shop products the Category component should be rendered

--- src/components/Shop/Filters/ShopFilter.jsx
+++ src/components/Shop/Filters/ShopFilter.jsx
@@ -2,12 +2,13 @@ import Letter from "./Letter";
 import Price from "./Price";
 import Category from "./Category";
 
-const ShopFilter = () => {
+const ShopFilter = (props) => {
   return (
     <section className="pt-6 pb-24 bg-neutral-100">
       <div className="container flex items-center justify-between gap-20">
         <div className="flex items-center gap-10">
           <Letter />
+          {props.productId != 0 && <Category productId={props.productId} />}
         </div>
         <div>
           <Price />
