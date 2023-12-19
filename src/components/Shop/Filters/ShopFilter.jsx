8d93476b34fbc0ac77a89ed88c564e0a60ed0c15
FrontEndDevo <mylinkacco@gmail.com>
2023-12-19 02:10:47 +0200

    Add the reset button to ShopFilter component and handle clicking it

--- src/components/Shop/Filters/ShopFilter.jsx
+++ src/components/Shop/Filters/ShopFilter.jsx
@@ -2,10 +2,20 @@ import Letter from "./Letter";
 import Price from "./Price";
 import Category from "./Category";
 import Area from "./Area";
+import { useDispatch, useSelector } from "react-redux";
+import { filtersActions } from "../../../redux/slices/filtersSlice";
 
 const ShopFilter = (props) => {
+  const filters = useSelector((s) => s.filters);
+  console.log(filters);
+
+  const dispatch = useDispatch();
+  const resetFiltersHandler = () => {
+    dispatch(filtersActions.resetFilters());
+  };
+
   return (
-    <section className="py-6 my-6 bg-neutral-100">
+    <section className="relative py-6 my-6 bg-neutral-100">
       <div className="container flex items-center justify-between">
         <div className="flex items-center gap-8">
           <Letter />
@@ -15,6 +25,15 @@ const ShopFilter = (props) => {
         <div>
           <Price />
         </div>
+      </div>
+      <div
+        className="absolute cursor-pointer top-1/2 right-4"
+        onClick={resetFiltersHandler}
+      >
+        <span class="relative flex h-5 w-5">
+          <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-75"></span>
+          <span class="relative inline-flex rounded-full h-5 w-5 bg-red-500"></span>
+        </span>
       </div>
     </section>
   );
